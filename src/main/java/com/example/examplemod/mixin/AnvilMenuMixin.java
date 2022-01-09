00aa2358454126fcc75079c4ada5c81ab2e3c21b
Alexandria <alxpettit@gmail.com>
2022-01-09 01:18:10 -0800

    first

---
+++ src/main/java/com/example/examplemod/mixin/AnvilMenuMixin.java
@@ -0,0 +1,20 @@
+package com.example.examplemod.mixin;
+
+import net.minecraft.world.inventory.AnvilMenu;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(AnvilMenu.class)
+public class AnvilMenuMixin {
+    @ModifyConstant(method = "createResult", constant = @Constant(intValue = 40))
+    private int customLimitInt(int i) {
+        return 99999;
+    }
+
+    @ModifyConstant(method = "createResult", constant = @Constant(intValue = 39))
+    private int customMaxInt(int i) {
+        return 99999 - 1;
+    }
+}
---
+++ src/main/java/com/example/examplemod/mixin/AnvilScreenMixin.java
@@ -0,0 +1,14 @@
+package com.example.examplemod.mixin;
+
+import net.minecraft.client.gui.screens.inventory.AnvilScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(AnvilScreen.class)
+public class AnvilScreenMixin {
+    @ModifyConstant(method = "renderLabels", constant = @Constant(intValue = 40))
+    private int customLimitInt(int i) {
+        return 99999;
+    }
+}
